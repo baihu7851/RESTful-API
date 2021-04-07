361b96ef15c8aba667cb3f636e60a5eff033cc81
DESKTOP-9335FOI\PC16 <baihu7851@gmail.com>
2021-04-07 13:20:14 +0800

    新增 UserSqlServer

---
+++ RESTfulAPI.Repository/Interfaces/IUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RESTfulAPI.Model.Models;
+
+namespace RESTfulAPI.Repository.Interfaces
+{
+    public interface IUser
+    {
+        IEnumerable<User> Users();
+
+        User User(int id);
+
+        void Add(IEnumerable<User> user);
+
+        void Update(IEnumerable<User> user);
+
+        void Delete(IEnumerable<int> id);
+    }
+}
---
+++ RESTfulAPI.Repository/Repositories/UserSqlServerRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using RESTfulAPI.Model.Models;
+using RESTfulAPI.Repository.Interfaces;
+
+namespace RESTfulAPI.Repository.Repositories
+{
+    public class UserSqlServerRepository : IUser
+    {
+        public UserSqlServerRepository(IConfiguration configuration, IDbConnection connection)
+        {
+            Configuration = configuration;
+            Connection = connection;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IDbConnection Connection { get; }
+
+        public void Add(IEnumerable<User> user)
+        {
+            const string strSql = "INSERT INTO [Users] (UserName, Birthday, Email, Phone) VALUES (@UserName, @Birthday, @Email, @Phone)";
+            foreach (var i in user)
+            {
+                Connection.ExecuteScalar<User>(strSql, i);
+            }
+        }
+
+        public void Delete(IEnumerable<int> id)
+        {
+            const string strSql = "DELETE FROM [Users] WHERE (Id = @Id)";
+            foreach (var i in id)
+            {
+                Connection.ExecuteScalar<User>(strSql, new { Id = i });
+            }
+        }
+
+        public void Update(IEnumerable<User> user)
+        {
+            const string strSql = "UPDATE  [Users] SET UserName = @UserName, Birthday = @Birthday, Email = @Email, Phone = @Phone WHERE (Id = @Id)";
+            foreach (var i in user)
+            {
+                Connection.ExecuteScalar<User>(strSql, i);
+            }
+        }
+
+        public User User(int id)
+        {
+            const string strSql = "SELECT * FROM [Users] WHERE (Users.Id = @Id)";
+            return Connection.QueryFirst<User>(strSql, new
+            {
+                Id = id
+            });
+        }
+
+        public IEnumerable<User> Users()
+        {
+            const string strSql = "SELECT * FROM [Users]";
+            return Connection.Query<User>(strSql);
+        }
+    }
+}
